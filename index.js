6ab5dc5a6d727bfbcfb8128267ce0529fddc71e8
Bhanutej <bhanutej.kalla89@gmail.com>
2021-10-18 16:54:21 +0530

    google authentication

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+const authRoutes = require("./routes/authRoutes");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", () => {
-  res.send({ ht: "there" });
-});
+app.use(
+  cookieSession({
+    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+authRoutes(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
