5c0db936c53559afc931a7662838d81fa9c3a4de
Bhanutej <bhanutej.kalla89@gmail.com>
2021-10-18 17:33:41 +0530

    added admin_client react app with proxy setup

---
+++ admin_client/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        hi
+        <a href="/auth/google">Google Auth</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
