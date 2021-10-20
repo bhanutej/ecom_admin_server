2d46e695be974a8b27a294e3d3eae48b1421bab3
Bhanutej <bhanutej.kalla89@gmail.com>
2021-10-20 08:55:00 +0530

    removed react, redux

--- admin_client/src/components/App.js
+++ admin_client/src/components/App.js
@@ -1,28 +1,13 @@
-import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import * as actions from "../actions";
 
-class App extends Component {
-  componentDidMount() {
-    this.props.fetchuser();
-  }
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>Hi</div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        <BrowserRouter>
-          <div>Hi</div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    fetchuser: () => dispatch(actions.fetchUser()),
-  };
-}
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
